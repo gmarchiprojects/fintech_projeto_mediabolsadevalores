713f6fa2802d3bcf1b22ecbfe0a0cc97de83a78c
Giovanni Marchi <giovanni.marchi92@gmail.com>
2020-07-17 13:01:23 -0300

    Intro pronta

--- media_mensal_bolsadevalores.java
+++ media_mensal_bolsadevalores.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 
 public class media_mensal_bolsadevalores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Introdução
+		System.out.print("Qual o seu nome?");
+		String user = sc.nextLine();
+		System.out.println("Seja bem vindo ao sistema de controle de ações da MarchiFintech");
+		System.out.println("Sr(a). "+user+", nosso sistema funciona, da seguinte maneira.");
+		System.out.println("Você ira preencher os dados de cada dia seguidos, iniciando no dia 1 até o dia 28,");
+		System.out.println("Após isso, o sistema irá calcular as médias semanais e mensais, sendo calculada tanto a maior quanto a menor");
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
